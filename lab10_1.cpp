3d97c2eb02c43d5c5bc099ca43b05c4454022aad
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-01-12 16:34:59 +0700

    Update lab10_1.cpp

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,17 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    srand(time(0));
+    cin.get();
+    cin.get();
+    cin.get();
+    string x[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    int i = rand()%9;
+    cout << "You will get " << x[i] << " " << "in this 261102." ;
+    return 0;
+}
